31108c422e70dc206bbf464291f4f8c040de8526
Akash Shrivastva <akash.shrivastva@gmail.com>
2024-04-28 04:16:21 -0400

    update train model

--- train_model.py
+++ train_model.py
@@ -6,6 +6,8 @@ from transformers import AutoProcessor, HubertModel
 from audio_dataset import AudioDataset
 from audio_downsampler import AudioDownsample
 
+import os
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 processor = AutoProcessor.from_pretrained("facebook/hubert-large-ls960-ft", cache_dir="./models")
@@ -14,29 +16,12 @@ text_model = SentenceTransformer('paraphrase-MiniLM-L6-v2', cache_folder='./mode
 
 # Training params
 AUDIO_INPUT_CAP = 10000
-NUM_EPOCHS = 100
+NUM_EPOCHS = 10
 BATCH_SIZE = 32
 CSV_FILE = "./vggsound.csv"
 DATA_DIR = "/scratch/as18464/data/audio"
 START_EPOCH = 0
 CHECKPOINT_DIR = './checkpoints'
-
-# Audio Downsampler
-audio_downsample = AudioDownsample().to(device)
-
-# Optimizer
-optimizer = torch.optim.AdamW(audio_downsample.parameters(), lr=5e-3, weight_decay=1e-5)
-
-# Scheduler
-lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-    optimizer,
-    mode="min",
-    patience=3,
-    factor=0.5,
-    threshold_mode="rel",
-    min_lr=1e-8,
-    threshold=0.01,
-)
 
 # Loss Calculator
 criterion = torch.nn.MSELoss()
@@ -50,13 +35,31 @@ dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 
 def train(start = 0, num_epochs=10):
 
-    checkpoint_path = f'{CHECKPOINT_DIR}/checkpoint_epoch{epoch}.pth'
-    
+    checkpoint_path = f'{CHECKPOINT_DIR}/checkpoint_epoch{start}.pth'
+
+    # Audio Downsampler
+    audio_downsample = AudioDownsample().to(device)
+
+    # Optimizer
+    optimizer = torch.optim.AdamW(audio_downsample.parameters(), lr=5e-3, weight_decay=1e-5)
+
+    # Scheduler
+    lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer,
+        mode="min",
+        patience=3,
+        factor=0.5,
+        threshold_mode="rel",
+        min_lr=1e-8,
+        threshold=0.01,
+    )
+
     if os.path.exists(checkpoint_path):
         checkpoint = torch.load(checkpoint_path)
-        audio_downsample = AudioDownsample()
+        # audio_downsample = AudioDownsample()
         audio_downsample.load_state_dict(checkpoint['model_state_dict']).to(device)
         optimizer = torch.optim.AdamW(checkpoint['optimizer_state_dict'])
+        loss = checkpoint['train_loss']
 
     for epoch in range(num_epochs):
         for audios, labels in dataloader:
@@ -87,9 +90,9 @@ def train(start = 0, num_epochs=10):
 
         torch.save({
             'epoch': epoch,
-            'model_state_dict': model.state_dict(),
+            'model_state_dict': audio_downsample.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
-            'train_loss': train_loss
+            'train_loss': loss
         }, checkpoint_path)
 
     print('Training finished!')
