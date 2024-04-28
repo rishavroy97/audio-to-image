6ea1599b8a7bde834b7741fd4bdc58b1815fbbcd
rishavroy97 <rishavroy008@gmail.com>
2024-04-28 17:01:17 -0400

    Use CLIP Text Tokenizer

--- audio_downsampler.py
+++ audio_downsampler.py
@@ -3,18 +3,20 @@ import torch.nn as nn
 
 class AudioDownsample(nn.Module):
     def __init__(self):
-        # super().__init__()
         super(AudioDownsample, self).__init__()
 
-        hidden_size = 31 * 1024
-        embedding_size = 384
-        self.proj1 = nn.Linear(hidden_size, hidden_size)
-        self.proj2 = nn.Linear(hidden_size, hidden_size)
-        self.proj3 = nn.Linear(hidden_size, embedding_size)
+        input_size = 31 * 1024
+        embedding_size = 77 * 1024
+        self.proj1 = nn.Linear(input_size, 512)
+        self.proj2 = nn.Linear(512, 256)
+        self.proj3 = nn.Linear(256, embedding_size)
         self.activation = nn.GELU()
 
     def forward(self, x):
-        x = x.view(x.size(0), -1)
+        batch_size = x.size(0)
+        x = x.view(batch_size, -1)
         x = self.activation(self.proj1(x))
         x = self.activation(self.proj2(x))
-        return self.proj3(x)
+        x = self.proj3(x)
+
+        return x.view(batch_size, 77, 1024)
--- train_model.py
+++ train_model.py
@@ -2,22 +2,24 @@ import argparse
 import os
 
 import torch
-from sentence_transformers import SentenceTransformer
 from torch.utils.data import DataLoader
 from transformers import AutoProcessor, HubertModel
+from transformers import CLIPTokenizer, CLIPTextModel
 
 from audio_dataset import AudioDataset
 from audio_downsampler import AudioDownsample
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+# external models
 processor = AutoProcessor.from_pretrained("facebook/hubert-large-ls960-ft", cache_dir="./models")
 audio_model = HubertModel.from_pretrained("facebook/hubert-large-ls960-ft", cache_dir="./models").to(device)
-text_model = SentenceTransformer('paraphrase-MiniLM-L6-v2', cache_folder='./models')
+clip_model = CLIPTextModel.from_pretrained("laion/CLIP-ViT-H-14-laion2B-s32B-b79K", cache_dir="./models").to(device)
+clip_tokenizer = CLIPTokenizer.from_pretrained("laion/CLIP-ViT-H-14-laion2B-s32B-b79K", cache_dir="./models")
 
 # Training params
 AUDIO_INPUT_CAP = 10000
-NUM_EPOCHS = 1000
+NUM_EPOCHS = 50
 BATCH_SIZE = 32
 CSV_FILE = "./vggsound.csv"
 DATA_DIR = "./data/audio"
@@ -42,13 +44,17 @@ def forward_pass(audios, labels, down_sampler):
     audios = audios.squeeze(1)
     labels = list(labels)
 
+    # Audio processing
     processed_audios = processor(audios, return_tensors="pt", sampling_rate=16000).input_values.squeeze(0).to(
         device)
     audio_embeddings = audio_model(processed_audios[:, :AUDIO_INPUT_CAP]).last_hidden_state.to(device)
     audio_ds_output = down_sampler(audio_embeddings).to(device)
 
-    text = text_model.encode(labels)
-    prompt_embeddings = torch.tensor(text).to(device)
+    # image processing
+    tokens = clip_tokenizer(labels, padding="max_length", max_length=77, truncation=True, return_tensors="pt")
+    text_tokens = tokens.input_ids.to(device)
+    attention_mask = tokens.attention_mask.to(device)
+    prompt_embeddings = clip_model(text_tokens, attention_mask=attention_mask).last_hidden_state.to(device)
 
     return audio_ds_output, prompt_embeddings
 
@@ -92,10 +98,10 @@ def train(start=0, num_epochs=10):
     checkpoint_path = f'{CHECKPOINT_DIR}/checkpoint_epoch{start}.pth'
     if start > 0 and os.path.exists(checkpoint_path):
         checkpoint = torch.load(checkpoint_path)
+        start = checkpoint['epoch']
         audio_downsample.load_state_dict(checkpoint['model_state_dict'])
         optimizer = torch.optim.AdamW(checkpoint['optimizer_state_dict'])
-        loss = checkpoint['train_loss']
-        start = checkpoint['epoch']
+        train_loss = checkpoint['train_loss']
         print(f'Loaded checkpoint at epoch {start}')
 
     # Load best loss
@@ -105,14 +111,16 @@ def train(start=0, num_epochs=10):
         best_model = torch.load(best_model_path)
         best_loss = best_model['loss']
 
-    for epoch in range(start, num_epochs):
+    train_losses = []
+    val_losses = []
+    val_loss = float('inf')
 
+    for epoch in range(start, num_epochs):
         # Training
         audio_downsample.train()
         for audios, labels in train_dataloader:
             # Forward Pass
             audio_ds_output, prompt_embeddings = forward_pass(audios, labels, audio_downsample)
-
             train_loss = criterion(audio_ds_output, prompt_embeddings)
 
             # Backward pass and optimization
@@ -125,22 +133,25 @@ def train(start=0, num_epochs=10):
         # Validation phase
         val_loss = calculate_validation_loss(audio_downsample, val_dataloader)
 
-        # Save the best model every 20 epochs
-        if epoch % 20 == 0 and val_loss < best_loss:
-            best_loss = val_loss
+        val_losses.append(val_loss)
+        train_losses.append(train_loss.item())
 
-            # Save the best model
-            if not os.path.exists(BEST_MODEL_DIR):
-                os.makedirs(BEST_MODEL_DIR)
+        # Save the best model every 10 epochs
+        if epoch % 10 == 0:
+            if val_loss < best_loss:
+                best_loss = val_loss
 
-            torch.save({
-                'epoch': epoch,
-                'model_state_dict': audio_downsample.state_dict(),
-                'loss': best_loss,
-            }, best_model_path)
-            print(f'Best model saved at epoch {epoch}')
+                # Save the best model
+                if not os.path.exists(BEST_MODEL_DIR):
+                    os.makedirs(BEST_MODEL_DIR)
 
-        if epoch % 100 == 0:
+                torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': audio_downsample.state_dict(),
+                    'loss': best_loss,
+                }, best_model_path)
+                print(f'Best model saved at epoch {epoch}')
+
             # Print loss every few epochs
             print(f'Epoch [{epoch}/{num_epochs}], Loss: {val_loss:.4f}')
 
@@ -161,12 +172,36 @@ def train(start=0, num_epochs=10):
     print('Training finished!')
     print(f'Final Loss: {val_loss:.4f}')
 
+    return train_losses, val_losses
+
+
+def save_losses(train_losses, val_losses, start_epoch, num_epochs):
+    tl = torch.tensor(train_losses)
+    vl = torch.tensor(val_losses)
+
+    checkpoint_path = f'{CHECKPOINT_DIR}/checkpoint_epoch{start_epoch}.pth'
+    if start_epoch > 0 and os.path.exists(checkpoint_path):
+        checkpoint = torch.load(checkpoint_path)
+        start_epoch = checkpoint['epoch']
+
+    if not os.path.exists(BEST_MODEL_DIR):
+        os.makedirs(BEST_MODEL_DIR)
+
+    torch.save({
+        'train_losses': tl,
+        'validation_losses': vl,
+        'start': start_epoch,
+        'num_epochs': num_epochs
+    }, f"{BEST_MODEL_DIR}/model-losses.pth")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Train Model for 1000 Epoch")
     parser.add_argument("--start", type=int, default=START_EPOCH, help="Starting index epoch")
     args = parser.parse_args()
 
-    print(f"Training Dataset Size: {train_dataset.__len__()}")
-    print(f"Validation Dataset Size: {val_dataset.__len__()}")
-    train(args.start, NUM_EPOCHS)
+    print(f"Training Dataset Size: {len(train_dataset)}")
+    print(f"Validation Dataset Size: {len(val_dataset)}")
+
+    train_losses, val_losses = train(args.start, NUM_EPOCHS)
+    save_losses(train_losses, val_losses, args.start, NUM_EPOCHS)
